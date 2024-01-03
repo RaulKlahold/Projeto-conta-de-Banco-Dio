d6e6f71f3a16afbcbd03a125b66923f3a5bf69d3
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-02 21:16:22 -0300

    commit att

--- ContaBanco/src/com/github/raulklahold/ContaBanco/ColetarDados.java
+++ ContaBanco/src/com/github/raulklahold/ContaBanco/ColetarDados.java
@@ -1,5 +1,36 @@
 package com.github.raulklahold.ContaBanco;
 
 public class ColetarDados {
+	private int NumeroConta;
+	private String Agencia;
+	private String NomeCliente;
+	private double Saldo;
+	public int getNumeroConta() {
+		return NumeroConta;
+	}
+	public void setNumeroConta(int numeroConta) {
+		NumeroConta = numeroConta;
+	}
+	public String getAgencia() {
+		return Agencia;
+	}
+	public void setAgencia(String agencia) {
+		Agencia = agencia;
+	}
+	public String getNomeCliente() {
+		return NomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		NomeCliente = nomeCliente;
+	}
+	public double getSaldo() {
+		return Saldo;
+	}
+	public void setSaldo(double saldo) {
+		Saldo = saldo;
+	}
+	
+	
+	
 
 }
--- ContaBanco/src/com/github/raulklahold/ContaBanco/ContaTerminal.java
+++ ContaBanco/src/com/github/raulklahold/ContaBanco/ContaTerminal.java
@@ -1,8 +1,17 @@
 package com.github.raulklahold.ContaBanco;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
+		
+		ColetarDados Conta = new ColetarDados();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o peso em kg: ");
+        int numero = scanner.nextInt();
+		Conta.setNumeroConta(numero);
 		
 
 	}
